d55dabd4e540d22f8229d6998f57b5df23af6053
WiktorLigeza <wiktorligeza@gmail.com>
2021-04-25 13:40:37 +0000

    copy

---
+++ copy/.ipynb_checkpoints/model_prep-Copy1-checkpoint.py
@@ -0,0 +1,132 @@
+#!/usr/bin/env python
+# coding: utf-8
+# %%
+
+# %%
+
+
+import tensorflow.keras as keras
+import matplotlib.pyplot as plt
+
+
+# # Generates CNN model
+
+# :param input_shape (tuple): Shape of input set  
+# :return model: CNN model
+
+# %%
+
+
+def build_CNN_model(input_shape):
+
+    # build network topology
+    model = keras.Sequential()
+
+    # 1st conv layer
+    model.add(keras.layers.Conv2D(32, (3, 3), activation='relu', input_shape=input_shape))
+    model.add(keras.layers.MaxPooling2D((3, 3), strides=(2, 2), padding='same'))
+    model.add(keras.layers.BatchNormalization())
+
+    # 2nd conv layer
+    model.add(keras.layers.Conv2D(32, (3, 3), activation='relu'))
+    model.add(keras.layers.MaxPooling2D((3, 3), strides=(2, 2), padding='same'))
+    model.add(keras.layers.BatchNormalization())
+
+    # 3rd conv layer
+    model.add(keras.layers.Conv2D(32, (2, 2), activation='relu'))
+    model.add(keras.layers.MaxPooling2D((2, 2), strides=(2, 2), padding='same'))
+    model.add(keras.layers.BatchNormalization())
+
+    # flatten output and feed it into dense layer
+    model.add(keras.layers.Flatten())
+    model.add(keras.layers.Dense(64, activation='relu'))
+    model.add(keras.layers.Dropout(0.3))
+
+    # output layer
+    model.add(keras.layers.Dense(10, activation='softmax'))
+
+    return model
+
+
+# :param input_shape (tuple): Shape of input set  
+# :return model: RNN-LSTM model
+
+# %%
+
+
+def build_RNN_LSTM_model(input_shape):
+    # build network topology
+    model = keras.Sequential()
+
+    # 2 LSTM layers
+    model.add(keras.layers.LSTM(64, input_shape=input_shape, return_sequences=True))
+    model.add(keras.layers.LSTM(64))
+
+    # dense layer
+    model.add(keras.layers.Dense(64, activation='relu'))
+    model.add(keras.layers.Dropout(0.3))
+
+    # output layer
+    model.add(keras.layers.Dense(10, activation='softmax'))
+
+    return model
+
+
+# # Predict a single sample using the trained model
+
+# :param model: Trained classifier  
+# :param X: Input data  
+# :param y (int): Target  
+
+# %%
+
+
+def predict(model, X, y):
+
+    # add a dimension to input data for sample - model.predict() expects a 4d array in this case
+    X = X[np.newaxis, ...] # array shape (1, 130, 13, 1)
+
+    # perform prediction
+    prediction = model.predict(X)
+
+    # get index with max value
+    predicted_index = np.argmax(prediction, axis=1)
+
+    print("Target: {}, Predicted label: {}".format(y, predicted_index))
+
+
+# # Plots accuracy/loss for training/validation set as a function of the epochs
+
+# :param history: Training history of model  
+# :return: fig.show
+
+# %%
+
+
+def plot_history(history):
+
+    fig, axs = plt.subplots(2)
+
+    # create accuracy sublpot
+    axs[0].plot(history.history["accuracy"], label="train accuracy")
+    axs[0].plot(history.history["val_accuracy"], label="test accuracy")
+    axs[0].set_ylabel("Accuracy")
+    axs[0].legend(loc="lower right")
+    axs[0].set_title("Accuracy eval")
+
+    # create error sublpot
+    axs[1].plot(history.history["loss"], label="train error")
+    axs[1].plot(history.history["val_loss"], label="test error")
+    axs[1].set_ylabel("Error")
+    axs[1].set_xlabel("Epoch")
+    axs[1].legend(loc="upper right")
+    axs[1].set_title("Error eval")
+
+    plt.show()
+
+
+# %%
+
+
+
+
